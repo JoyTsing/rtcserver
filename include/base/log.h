91c3935438f48869f4798399cc031398a24770a3
JoyTsing <792566033@qq.com>
2024-02-21 00:23:17 +0800

    [fix] change

--- include/base/log.h
+++ include/base/log.h
@@ -1,8 +1,17 @@
 #pragma once
-#include <string>
 #include <rtc_base/logging.h>
+#include <string>
 namespace xrtc {
 class Log : public rtc::LogSink {
+  public:
+    Log(const std::string &log_dir,
+        const std::string &log_name,
+        const std::string &log_level);
+    ~Log() override;
+    void OnLogMessage(
+        const std::string &message, rtc::LoggingSeverity severity) override;
+    void OnLogMessage(const std::string &message) override;
+
   private:
     std::string _log_dir;
     std::string _log_name;
