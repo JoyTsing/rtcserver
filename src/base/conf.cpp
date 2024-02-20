41e21a2ae365b446647eda42a5801c6270862744
JoyTsing <792566033@qq.com>
2024-02-20 22:20:26 +0800

    [add]conf配置

--- src/base/conf.cpp
+++ src/base/conf.cpp
@@ -1,4 +1,5 @@
 #include "base/conf.h"
+#include "yaml-cpp/exceptions.h"
 #include <cstdio>
 #include <yaml-cpp/yaml.h>
 namespace xrtc {
@@ -11,8 +12,19 @@ int load_general_conf(const std::string &filename, GeneralConf *conf) {
     conf->log_name = filename;
     conf->log_level = "info";
     conf->log_to_stderr = true;
+    YAML::Node config = YAML::LoadFile(filename);
+    try {
+        conf->log_dir = config["log"]["log_dir"].as<std::string>();
+        conf->log_name = config["log"]["log_name"].as<std::string>();
+        conf->log_level = config["log"]["log_level"].as<std::string>();
+        conf->log_to_stderr = config["log"]["log_to_stderr"].as<bool>();
+    } catch (YAML::Exception &e) {
+        (void)fprintf(
+            stderr, "YAML::Exception line : %d,column: %d, error:%s\n",
+            e.mark.line + 1, e.mark.column + 1, e.what());
+        return -1;
+    }
 
-    YAML::Node node = YAML::LoadFile(filename);
     return 0;
 }
 } // namespace xrtc
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include "base/conf.h"
 #include <cstdio>
-
+#include <unistd.h>
 xrtc::GeneralConf *conf = nullptr;
 
 int init_general_conf(const std::string &conf_file) {
-    if (conf_file.empty() || conf == nullptr) {
-        (void)fprintf(stderr, "conf_file or conf is nullptr\n");
+    if (conf_file.empty() || conf != nullptr) {
+        char *buffer = getcwd(NULL, 0);
+        (void)fprintf(stderr, "%s no conf_file\n", buffer);
         return -1;
     }
     conf = new ::xrtc::GeneralConf();
