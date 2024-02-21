521e8322e2118da0951518115f1945b359a754b3
JoyTsing <792566033@qq.com>
2024-02-21 23:42:15 +0800

    [add] fin event_loop

--- include/base/event_loop.h
+++ include/base/event_loop.h
@@ -1,26 +1,76 @@
 #pragma once
-#include <libev/ev.h>
-namespace xrtc {
-class EventLoop;
-class IOWatcher;
-using io_callback_t =
-    void (*)(EventLoop *, IOWatcher *, int fd, int events, void *data);
 
+#include <libev/ev.h>
+#include <string>
+namespace xrtc {
 class EventLoop {
   public:
+    // 需要增加的话在这里即可，宏展开自动维护
+#define _TRANS_FOREACH(f) f(READ) f(WRITE)
+
+    enum {
+#define _FUNCTION(name) name,
+        _TRANS_FOREACH(_FUNCTION)
+#undef _FUNCTION
+    };
+
+    static inline std::string event_name(int event) {
+        switch (event) {
+#define _FUNCTION(ev) \
+    case ev:          \
+        return #ev;
+            _TRANS_FOREACH(_FUNCTION)
+#undef _FUNCTION
+            default:
+                break;
+        }
+        return "unknown";
+    }
+#undef _TRANS_FOREACH
+
+  public:
+    class IOWatcher;
+    using io_callback_t =
+        void (*)(EventLoop *, IOWatcher *, int fd, int events, void *data);
+
     class IOWatcher {
       public:
         IOWatcher(EventLoop *el, io_callback_t call, void *data)
-            : el(el), call(call), data(data) {
-            io.data = this;
+            : event_loop(el), call(call), data(data) {
+            io = new ev_io;
+            io->data = this;
         }
 
       public:
-        ev_io io;
+        ev_io *io;
 
-        EventLoop *el;
+        EventLoop *event_loop;
         io_callback_t call;
         void *data;
+    };
+
+  public:
+    class TimeWatcher;
+    using time_callback_t = void (*)(EventLoop *, TimeWatcher *w, void *data);
+
+    class TimeWatcher {
+      public:
+        TimeWatcher(
+            EventLoop *event_loop,
+            time_callback_t call,
+            void *data,
+            bool repeate)
+            : event_loop(event_loop), call(call), data(data), repeate(repeate) {
+            timer = new ev_timer;
+            timer->data = this;
+        }
+
+      public:
+        EventLoop *event_loop;
+        struct ev_timer *timer;
+        time_callback_t call;
+        void *data;
+        bool repeate;
     };
 
   public:
@@ -31,6 +81,21 @@ class EventLoop {
     void stop();
 
     IOWatcher *CreateIoEvent(io_callback_t call, void *data);
+    void StartIOEvent(IOWatcher *w, int fd, int mask);
+    void StopIOEvent(IOWatcher *w, int fd, int mask);
+    void DeleteIOEvent(IOWatcher *w);
+
+    TimeWatcher *CreateTimer(time_callback_t call, void *data, bool repeate);
+    void StartTimer(TimeWatcher *w, unsigned int msec);
+    void StopTimer(TimeWatcher *w);
+    void DeleteTimer(TimeWatcher *w);
+
+  private:
+    static void
+    generic_io_callback(struct ev_loop *loop, struct ev_io *w, int events);
+
+    static void generic_time_callback(
+        struct ev_loop *loop, struct ev_timer *timer, int events);
 
   private:
     struct ev_loop *_loop;
--- src/base/event_loop.cpp
+++ src/base/event_loop.cpp
@@ -1,6 +1,30 @@
 #include "base/event_loop.h"
-#include "libev/ev.h"
+#include <libev/ev.h>
 namespace xrtc {
+
+int trans_to_libev(int event) {
+    switch (event) {
+        case EventLoop::READ:
+            return EV_READ;
+        case EventLoop::WRITE:
+            return EV_WRITE;
+        default:
+            break;
+    }
+    return 0;
+}
+
+int trans_from_libev(int event) {
+    switch (event) {
+        case EV_READ:
+            return EventLoop::READ;
+        case EV_WRITE:
+            return EventLoop::WRITE;
+        default:
+            break;
+    }
+    return 0;
+}
 
 EventLoop::EventLoop(void *owner) : owner(owner) {
     _loop = ev_loop_new(EVFLAG_AUTO);
@@ -18,6 +42,92 @@ void EventLoop::stop() {
     ev_break(_loop, EVBREAK_ALL);
 }
 
-EventLoop::IOWatcher *EventLoop::CreateIoEvent(io_callback_t call, void *data) {
+void EventLoop::generic_io_callback(
+    struct ev_loop * /*loop*/, struct ev_io *w, int events) {
+    auto *watcher = (IOWatcher *)w->data;
+    watcher->call(
+        watcher->event_loop, watcher, w->fd, trans_from_libev(events), w->data);
 }
+
+void EventLoop::generic_time_callback(
+    struct ev_loop * /*loop*/, struct ev_timer *timer, int /*events*/) {
+    auto *watcher = (TimeWatcher *)(timer->data);
+    watcher->call(watcher->event_loop, watcher, watcher->data);
+}
+
+// io_event
+EventLoop::IOWatcher *EventLoop::CreateIoEvent(io_callback_t call, void *data) {
+    auto *w = new IOWatcher(this, call, data);
+    ev_init(w->io, generic_io_callback);
+    return w;
+}
+
+void EventLoop::StartIOEvent(IOWatcher *w, int fd, int mask) {
+    struct ev_io *io = w->io;
+    if (ev_is_active(io)) {
+        int active_events = trans_from_libev(io->events);
+        int new_events = active_events | mask;
+        if (new_events == active_events) { return; }
+        // add
+        int event = trans_to_libev(new_events);
+        ev_io_stop(_loop, io);
+        ev_io_set(io, fd, event);
+        ev_io_start(_loop, io);
+    }
+    // new event
+    int event = trans_to_libev(mask);
+    ev_io_set(io, fd, event);
+    ev_io_start(_loop, io);
+}
+
+void EventLoop::StopIOEvent(IOWatcher *w, int fd, int mask) {
+    struct ev_io *io = w->io;
+    int active_events = trans_from_libev(io->events);
+    int events = active_events & (~mask);
+
+    if (events == active_events) { return; }
+    events = trans_to_libev(events);
+    ev_io_stop(_loop, io);
+    if (events != EV_NONE) {
+        ev_io_set(io, fd, events);
+        ev_io_start(_loop, io);
+    }
+}
+
+void EventLoop::DeleteIOEvent(IOWatcher *w) {
+    struct ev_io *io = w->io;
+    ev_io_stop(_loop, io);
+    delete w;
+}
+
+// time event
+EventLoop::TimeWatcher *
+EventLoop::CreateTimer(time_callback_t call, void *data, bool repeate) {
+    auto *watcher = new TimeWatcher(this, call, data, repeate);
+    ev_init(watcher->timer, generic_time_callback);
+    return watcher;
+}
+
+void EventLoop::StartTimer(TimeWatcher *w, unsigned int msec) {
+    struct ev_timer *timer = w->timer;
+    double sec = double(msec) / 1e6;
+    if (!w->repeate) {
+        ev_timer_set(timer, sec, 0);
+        ev_timer_start(_loop, timer);
+        return;
+    }
+    timer->repeat = sec;
+    ev_timer_again(_loop, timer);
+}
+
+void EventLoop::StopTimer(TimeWatcher *w) {
+    struct ev_timer *timer = w->timer;
+    ev_timer_stop(_loop, timer);
+}
+
+void EventLoop::DeleteTimer(TimeWatcher *w) {
+    StopTimer(w);
+    delete w;
+}
+
 } // namespace xrtc
